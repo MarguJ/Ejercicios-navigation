5d91939da41073e2ea7aa1a7511a73af9c6361c0
MarguJ <margulesjoa@gmail.com>
2025-09-01 10:45:04 -0300

    RepoGod

---
+++ Assets/AgentScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentScript : MonoBehaviour
+{
+    [SerializeField] Transform target; // Asignar en el inspector el objetivo al que debe moverse
+    private NavMeshAgent agent;
+    [SerializeField] Animator anim;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+            agent.SetDestination(target.position);
+        anim.SetFloat("Speed",agent.velocity.magnitude);
+    }
+}
